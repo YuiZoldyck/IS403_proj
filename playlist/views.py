73bb65b08baf44ef0688e895a319728d2aa06d20
YuiZoldyck <betasan28@gmail.com>
2022-12-12 09:55:22 -0700

    comments

--- playlist/views.py
+++ playlist/views.py
@@ -4,10 +4,11 @@ from django.shortcuts import render, redirect
 from .models import Songs, Category
 
 # Create your views here.
+# main page
 def indexPageView(request):
     return render(request, 'index.html')
 
-
+# page to view all existing songs in playlist
 def viewPageView(request):
     songs = Songs.objects.all().values()
     categories = Category.objects.all()
@@ -17,6 +18,7 @@ def viewPageView(request):
     }
     return render(request, 'displaySongs.html', {'data':context})
 
+# link to reorder songs within playlist by categories
 def reorderTable(request): 
     order = request.GET.get('orderby')
     songs = Songs.objects.all().order_by(order).values()
@@ -27,9 +29,11 @@ def reorderTable(request):
     }
     return render(request, 'reorder.html', {'data':context})
 
+# link to add new song into playlist
 def createPageView(request):
     return render(request, 'addSong.html')
 
+# link to redirect to home after adding somg
 def addRecordView(request):
     if request.method == 'POST':
         name = request.POST['SongName']
@@ -48,6 +52,7 @@ def addRecordView(request):
 
     return redirect(indexPageView)
 
+# link to update existing song
 def editPageView(request, sid):
     song = Songs.objects.values().get(id=sid)
     context = {
@@ -55,6 +60,7 @@ def editPageView(request, sid):
     }
     return render(request, 'editSong.html', {'data':context})
 
+# link to save the new updates to song, redirect to home
 def submitChanges(request,sid):
     song = Songs.objects.get(id=sid)
 
@@ -72,7 +78,7 @@ def submitChanges(request,sid):
         song.save()
     return redirect(indexPageView)
     
-
+# delete song from playlist
 def deletePageView(request, sid):
     song = Songs.objects.get(id=sid).delete()
     return redirect(indexPageView)
